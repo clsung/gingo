76b9e8c014cce918652c0644a2b4c595dca1d296
Cheng-Lung Sung <clsung@gmail.com>
2016-09-27 13:34:27 +0800

    add docker-compose example, along with Dockerfiles

---
+++ docker/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+docker build -t clsung/gingo_build build/
+docker run -d --name gingo_build clsung/gingo_build
+docker cp gingo_build:/go/bin/gingo run/
+docker stop gingo_build
+docker rm gingo_build
+docker rmi clsung/gingo_build
+docker build -t clsung/gingo run/
