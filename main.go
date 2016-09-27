62bbfc97f18180c9738a6669bc0875c7111bb5c1
Cheng-Lung Sung <clsung@gmail.com>
2016-09-27 13:02:45 +0800

    gin + redis

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/garyburd/redigo/redis"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	redisAddress = flag.String("redis-address", ":6379", "Address to the Redis server")
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/ready", checkRedis)
+	router.GET("/ready/:auth", checkRedisWithAuth)
+
+	router.Run()
+}
+
+func checkRedisWithAuth(c *gin.Context) {
+	auth := c.Param("auth")
+	doRedis(c, "PING", auth)
+}
+
+func checkRedis(c *gin.Context) {
+	doRedis(c, "PING", "")
+}
+
+func doRedis(c *gin.Context, cmd, auth string) {
+	client, err := redis.Dial("tcp", *redisAddress)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"message": err,
+		})
+	}
+	defer client.Close()
+	if auth != "" {
+		if _, err := client.Do("AUTH", auth); err != nil {
+			c.JSON(400, gin.H{
+				"message": "Invalid password",
+			})
+			return
+		}
+	}
+	ret, err := client.Do(cmd)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"message": err,
+		})
+	}
+	c.JSON(200, gin.H{
+		"message": ret,
+	})
+}
