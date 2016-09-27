a1885abf3e48777c82c9b1616d191df662f1ae02
Cheng-Lung Sung <clsung@gmail.com>
2016-09-27 16:13:04 +0800

    add pooled version

--- docker/build.sh
+++ docker/build.sh
@@ -3,6 +3,7 @@
 docker build -t clsung/gingo_build build/
 docker run -d --name gingo_build clsung/gingo_build
 docker cp gingo_build:/go/bin/gingo run/
+docker cp gingo_build:/go/bin/gingo_nopool run/
 docker stop gingo_build
 docker rm gingo_build
 docker rmi clsung/gingo_build
